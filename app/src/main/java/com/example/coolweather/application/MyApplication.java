5ae73b4edb8714ba62612d6441453f3ae6687539
LF <1165339017@qq.com>
2018-06-04 17:32:01 +0800

    加入创建数据库和表的配置。

--- app/src/main/java/com/example/coolweather/application/MyApplication.java
+++ app/src/main/java/com/example/coolweather/application/MyApplication.java
@@ -3,6 +3,8 @@ package com.example.coolweather.application;
 import android.app.Application;
 import android.content.Context;
 
+import org.litepal.LitePal;
+
 /**
  * Created by Administrator on 2018/6/3.
  */
@@ -13,6 +15,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         context = getApplicationContext();
+        LitePal.initialize(context);
         super.onCreate();
     }
     public static Context getContext(){
